d86b89af5a2026225ed1a95b32b75adcd6416f2a
Lucas-Ballu <lucas0999.lb@gmail.com>
2025-12-12 11:56:15 +0100

    FEAT: ajout du dé dans la section game

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { GameSection } from "./gameSection.js";
 import { EndSection } from "./endSection.js";
+import { rollDice } from "./rollDice.js";
 
 GameSection();
 EndSection();
+rollDice();
